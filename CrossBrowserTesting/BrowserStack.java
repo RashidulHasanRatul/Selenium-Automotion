c2025493ab89346bd4358a46665aba33758a5ce3
Rashidul Hasan Ratul <49413046+RHRATUL@users.noreply.github.com>
2020-03-06 00:57:33 +0600

    Add files via upload
    Cross Browser

---
+++ CrossBrowserTesting/BrowserStack.java
@@ -0,0 +1,36 @@
+package CrossBrowserTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BrowserStack {
+	
+	public static final String USERNAME = "ratul11";
+	  public static final String AUTOMATE_KEY = "CDfze5Hgqs37QaXXpTUs";
+	  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+	  public static void main(String[] args) throws Exception {
+
+	    DesiredCapabilities caps = new DesiredCapabilities();
+	    caps.setCapability("browser", "Chrome");
+	    caps.setCapability("browser_version", "80.0"); 
+	    caps.setCapability("os", "Windows");
+	    caps.setCapability("os_version", "10");
+	    caps.setCapability("resolution", "1024x768");
+	    caps.setCapability("name", " Frist Cross Browser Test");
+
+	    WebDriver driver = new RemoteWebDriver(new java.net.URL(URL), caps);
+	    
+	    driver.get("http://www.google.com/ncr");
+	    WebElement element = driver.findElement(By.name("q"));
+
+	    element.sendKeys("BrowserStack");
+	    element.submit();
+
+	    System.out.println(driver.getTitle());
+	    driver.quit();
+}
+}
